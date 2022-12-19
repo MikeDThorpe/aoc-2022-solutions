e17e9de3616443425939b9ddfc5d05933db6ffbe
michaelthorpe <michael.thorpe@digital.homeoffice.gov.uk>
2022-12-19 10:11:41 +0000

    refactor solution

--- src/main/java/com/adventofcode/solutions/Main.java
+++ src/main/java/com/adventofcode/solutions/Main.java
@@ -14,13 +14,19 @@ public class Main {
         List<String> instruction = new ArrayList<>();
         while(sc.hasNextLine()) {
             String line = sc.nextLine();
+
             if(line.contains("$")) {
                 if (!instruction.isEmpty())
                     instructions.add(instruction);
                 instruction = new ArrayList<>();
             }
+
+
             instruction.add(line);
         }
+
+        instructions.add(instruction);
+        System.out.println(instructions);
 
         List<Directory> allDirectories = new ArrayList<>();
         List<com.adventofcode.solutions.File> allFiles = new ArrayList<>();
@@ -31,72 +37,66 @@ public class Main {
 
         Directory currentDirectory = homeDirectory;
 
-    for (List<String> ins : instructions) {
-      System.out.println(currentDirectory.getName());
-      String command = ins.get(0);
-      if (command.contains("cd")) {
+        for (List<String> ins : instructions) {
+            String command = ins.get(0);
+          if (command.contains("cd")) {
 
-        // get name from string
-        String[] commandParts = command.split("\\s+");
+            // get name from string
+            String[] commandParts = command.split("\\s+");
 
-        if (commandParts[2].equals("..")) {
-          currentDirectory = currentDirectory.getParentDirectory();
-          continue;
-        }
+            if (commandParts[2].equals("..")) {
+                if(currentDirectory.getParentDirectory() != null) {
+                    currentDirectory = currentDirectory.getParentDirectory();
+                }
+              continue;
+            }
 
-        Directory dir = doesDirectoryExist(commandParts[2], allDirectories);
+            Directory dir = doesDirectoryExist(commandParts[2], allDirectories);
 
-        if (dir == null) {
-          Directory newDirectory = new Directory();
-          newDirectory.setName(commandParts[2]);
-          newDirectory.setParentDirectory(currentDirectory);
-          allDirectories.add(newDirectory);
-          currentDirectory = newDirectory;
-        } else {
-          currentDirectory = dir;
-        }
-      } else {
-        // handle list command - show contents of directory
-        for (int i = 1; i < ins.size(); i++) {
-          String content = ins.get(i);
-          if (content.contains("dir")) {
-            String[] dirParts = content.split("\\s+");
-
-            Directory dir = doesDirectoryExist(dirParts[1], allDirectories);
             if (dir == null) {
               Directory newDirectory = new Directory();
-              newDirectory.setName(dirParts[1]);
+              newDirectory.setName(commandParts[2]);
               newDirectory.setParentDirectory(currentDirectory);
               allDirectories.add(newDirectory);
-              currentDirectory.addDirectory(newDirectory);
+              currentDirectory = newDirectory;
             } else {
-              currentDirectory.addDirectory(dir);
+              currentDirectory = dir;
             }
           } else {
-            // add files in list
-            String[] fileParts = content.split("\\s+");
-            com.adventofcode.solutions.File file = checkFileExists(fileParts[0], allFiles);
-            if (file == null) {
-              com.adventofcode.solutions.File newFile =
-                  new com.adventofcode.solutions.File(fileParts[1], fileParts[0]);
-              allFiles.add(newFile);
-              currentDirectory.addFile(newFile);
-            } else {
-              currentDirectory.addFile(file);
+            // handle list command - show contents of directory
+            for (int i = 1; i < ins.size(); i++) {
+              String content = ins.get(i);
+              if (content.contains("dir")) {
+                String[] dirParts = content.split("\\s+");
+
+                Directory dir = doesDirectoryExist(dirParts[1], allDirectories);
+                if (dir == null) {
+                  Directory newDirectory = new Directory();
+                  newDirectory.setName(dirParts[1]);
+                  newDirectory.setParentDirectory(currentDirectory);
+                  allDirectories.add(newDirectory);
+                  currentDirectory.addDirectory(newDirectory);
+                } else {
+                  currentDirectory.addDirectory(dir);
+                }
+              } else {
+                // add files in list
+                String[] fileParts = content.split("\\s+");
+                com.adventofcode.solutions.File file = checkFileExists(fileParts[0], allFiles);
+                if (file == null) {
+                  com.adventofcode.solutions.File newFile =
+                      new com.adventofcode.solutions.File(fileParts[1], fileParts[0]);
+                  allFiles.add(newFile);
+                  currentDirectory.addFile(newFile);
+                } else {
+                  currentDirectory.addFile(file);
+                }
+              }
             }
           }
         }
-      }
-    }
-
-//        List<Integer> filteredDirectories = allDirectories.stream()
-//                .map(Directory::calculateDirectorySize)
-//                .filter(ds -> ds < 100000)
-//                .collect(Collectors.toList());
-
-        allDirectories.forEach(
-                dir -> System.out.println(dir.calculateDirectorySize(dir.getDirectories()))
-        );
+        // calculate result
+        allDirectories.forEach(dir -> System.out.println(dir.calculateDirectorySize()));
     }
     public static Directory doesDirectoryExist(String name, List<Directory> directories) {
         Optional<Directory> dir = directories.stream()
